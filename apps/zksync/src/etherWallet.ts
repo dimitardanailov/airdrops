107bf5e1cc10ce2830ad431072bc1b1b6b669f05
Dimitar Danailov <dimityr.danailov@gmail.com>
2024-02-19 08:40:59 +0200

    The application is able to create ethers and zksync wallet

--- apps/zksync/src/etherWallet.ts
+++ apps/zksync/src/etherWallet.ts
@@ -1,6 +1,6 @@
-import {ethers} from 'ethers'
+import {ethers, HDNodeWallet} from 'ethers'
 
-function createWallet(): ethers.HDNodeWallet {
+function createWallet(): HDNodeWallet {
   const ethWallet = ethers.Wallet.createRandom()
   console.log('Ethereum Wallet - Address:', ethWallet.address)
   console.log('Ethereum Wallet - Private Key:', ethWallet.privateKey)
--- apps/zksync/src/main.ts
+++ apps/zksync/src/main.ts
@@ -1,5 +1,12 @@
 /* eslint-disable @typescript-eslint/no-extra-semi */
+import {ethers} from 'ethers'
+import {Wallet} from 'zksync-ethers'
 import createWallet from './etherWallet'
 ;(async () => {
-  createWallet()
+  const etherWallet = createWallet()
+
+  const ethProvider = ethers.getDefaultProvider('sepolia')
+  const wallet = Wallet.fromMnemonic(etherWallet.mnemonic.phrase, ethProvider)
+
+  console.log('wallet', wallet)
 })()
